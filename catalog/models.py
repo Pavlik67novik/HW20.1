c5a0a32a715446d0fe6a2b8998b26ba3ac71a6df
Pavel Novikov <pavlik.novik@yandex.ru>
2024-07-21 19:27:30 +0300

    add poetry

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(
         max_length=30,
@@ -62,4 +63,3 @@ class Meta:
 
 def __str__(self):
     return self.name
-
